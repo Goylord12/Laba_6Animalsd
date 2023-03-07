052db3426d231e340154eb676a2c06381a1e58f3
Goylord12 <96524736+Goylord12@users.noreply.github.com>
2023-03-07 10:02:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,15 +4,17 @@ class Animal
 {
     public string food;
     public string location;
+    public string voice;
 
-    public Animal(string food, string location)
+    public Animal(string food, string location, string voice)
     {
         this.food = food;
         this.location = location;
+        this.voice = voice;
     }
     public virtual void makeNoise()
     {
-        Console.WriteLine("Животное спит.");
+        Console.WriteLine("Животное шумит.");
     }
 
     public virtual void eat()
@@ -28,20 +30,19 @@ class Animal
 
 class Dog : Animal
 {
-    public Dog(string food, string location) : base(food, location)
+    public Dog(string food, string location, string voice) : base(food, location, voice)
     {
-        this.food = food;
-        this.location = location;
+    
     }
 
     public override void makeNoise()
     {
-        Console.WriteLine("Собака лает.");
+        Console.WriteLine($"Собака {voice}.");
     }
 
     public override void eat()
     {
-        Console.WriteLine("Собака кушает кость.");
+        Console.WriteLine($"Собака кушает {food}.");
 
     }
 
@@ -54,20 +55,19 @@ class Dog : Animal
 class Cat : Animal
 {
 
-    public Cat(string food, string location) : base(food, location)
+    public Cat(string food, string location, string voice) : base(food, location, voice)
     {
-        this.food = food;
-        this.location = location;
+
     }
 
     public override void makeNoise()
     {
-        Console.WriteLine("Кошка мяукает.");
+        Console.WriteLine($"Кошка {voice}.");
     }
 
     public override void eat()
     {
-        Console.WriteLine("Кошка ест рыбу.");
+        Console.WriteLine($"Кошка ест  {food}.");
     }
 
     public override void sleep()
@@ -78,20 +78,19 @@ class Cat : Animal
 
 class Horse : Animal
 {
-    public Horse(string food, string location) : base(food, location)
+    public Horse(string food, string location, string voice) : base(food, location, voice)
     {
-        this.food = food;
-        this.location = location;
+   
     }
 
     public override void makeNoise()
     {
-        Console.WriteLine("Лошадь ржет.");
+        Console.WriteLine($"Лошадь {voice}.");
     }
 
     public override void eat()
     {
-        Console.WriteLine("Лошадь ест сено.");
+        Console.WriteLine($"Лошадь ест {food}.");
     }
     public override void sleep()
     {
@@ -104,6 +103,7 @@ class Veterinarian
     public void treatAnimal(Animal animal)
     {
         Console.WriteLine("Животное: " + animal);
+        Console.WriteLine("Звук: " + animal.voice);
         Console.WriteLine("Еда: " + animal.food);
         Console.WriteLine("Местоположение: " + animal.location + "\n");
     }
@@ -113,17 +113,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Dog dog = new Dog("Мясо", "Вольер");
+        Dog dog = new Dog("мясо", "вольер", "лает");
         dog.makeNoise();
         dog.eat();
         dog.sleep();
 
-        Cat cat = new Cat("Корм", "Дом");
+        Cat cat = new Cat("корм", "дом", "мяукает");
         cat.makeNoise();
         cat.eat();
         cat.sleep();
 
-        Horse horse = new Horse("Овес", "Стойло");
+        Horse horse = new Horse("овес", "стойло", "ржет");
         horse.makeNoise();
         horse.eat();
         horse.sleep();
